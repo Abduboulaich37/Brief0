2aec1741adcbd108e479009d9629da556f8d082b
Abduboulaich37 <abduboulaich37@gmail.com>
2022-10-14 06:31:44 +0100

    Title

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- Title -->
+    <title>Brief 1 : Crud Promotion using PHP POO</title>
+</head>
+<body><br>
+    <!-- Name of Crud -->
+    <h1 class="text-center text-info">YourTasks</h1>
+</body>
+</html>
